4d97bb92684d38e578cbcf6e90e9cfc54e225761
Varun Kohli <kohlivarun5@users.noreply.github.com>
2022-01-26 21:24:40 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,8 @@ let package = Package(
         .library(name: "nearclientios", targets: ["nearclientios"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/yannickl/AwaitKit.git",from:"5.2.0")
+        .package(url: "https://github.com/yannickl/AwaitKit.git",from:"5.2.0"),
+        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.0.0"),
     ],
     targets: [
         .target(
